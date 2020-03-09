ad2ace16706ee487f2a0dcbbf164355984685506
ssaladd <58093617+ssaladd@users.noreply.github.com>
2020-03-08 21:40:44 -0400

    -

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
-$(function(){  // $(document).ready shorthand
-    $('.monster').fadeIn('slow');
-  });
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-  $(document).ready(function() {
-    $(window).scroll( function(){
-        $('.hideme').each( function(i){
-            
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1'},1000);
-            }
-            
-        }); 
-    });
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
